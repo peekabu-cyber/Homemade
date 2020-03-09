40d014b3f4f2a51a7ada7b910083e3bbbbe7c204
peekabu-cyber <61954741+peekabu-cyber@users.noreply.github.com>
2020-03-09 00:29:57 -0400

    Add files via upload

--- src/components/contact_form.php
+++ src/components/contact_form.php
@@ -1,17 +1,16 @@
 <?php
 if(isset($_POST['submit'])) {
-    echo "got here";
-    echo var_dump($_POST);
-    echo "got here";
     $name = $_POST['name'];
-    $email = $_POST['email'];
+    $email_from = $_POST['email'];
     $formcontent = $_POST['reason'];
-    echo "got here";
-    $recipient = "brukabu@gmail.com";
+
+    $to = "brukabu@gmail.com";
     $subject = "HomeMade - New Contact Form";
-    $message = "From: $name \n Message: $formcontent";
-    $mailheader = "From: $email \r\n";
-    mail($recipient, $subject, $message, $mailheader) or die("Error!");
-    echo "Thanks!";
+    $message = "From: $name. \n".
+        "Message: $formcontent \n";
+    $mailheader = "From: $email_from \r\n";
+    $mailheader .= "Reply-To: $name \r\n";
+
+    mail($to, $subject, $message, $mailheader) or die("Error!");
 }
 ?>

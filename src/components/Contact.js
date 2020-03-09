b1ba4e124df9f8a3d325e59e4f8b43335b0a211b
peekabu-cyber <61954741+peekabu-cyber@users.noreply.github.com>
2020-03-09 02:43:37 -0400

    Add files via upload

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,22 +2,79 @@ import React, {Component} from "react";
 import Nav from "./Nav";
 
 
-class Contact extends Component{
-	render(){
-		
-		return(
+class Contact extends Component {
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	// 		name: '',
+	// 		email: '',
+	// 		reason: ''
+	// 	}
+	// }
+	//
+	// handleSubmit(e) {
+	// 	const proxyurl = "https://cors-anywhere.herokuapp.com/";
+	//
+	// 	e.preventDefault();
+	// 	fetch('http://localhost:3001/send',{
+	// 		method: "POST",
+	// 		body: JSON.stringify(this.state),
+	// 		headers : {
+	// 			'Content-Type': 'application/json',
+	// 			'Accept': 'application/json'
+	// 		},
+	// 	}).then(
+	// 		(response) => (response.json())
+	// 	).then((response)=>{
+	// 		if (response.status === 'success'){
+	// 			alert("Message Sent.");
+	// 			this.resetForm()
+	// 		}else if(response.status === 'fail'){
+	// 			alert("Message failed to send.")
+	// 		}
+	// 	})
+	// }
+
+	render() {
+		return (
 			<div>
 				<Nav/>
 				<div id="contact-form">
-					<form action="./contact_form.php" method = "POST">
+					<form action="https://formspree.io/xdozrgyj" method="POST">
 						<input type="text" className="form_input contact_name" name="name" placeholder="Name" required/>
-						<input type = "email" className="form_input contact_email" name="email" placeholder="Email" required/>
-						<textarea type= "text" className="form-control" rows="10" name="reason" placeholder="Reason for email"> </textarea>
+						<input type="text" className="form_input contact_email" name="email" placeholder="Email"
+							   required/>
+						<textarea type="text" className="form-control" rows="10" name="reason"
+								  placeholder="Reason for email"> </textarea>
 						<button type="submit" className="btn btn-warning form_btn">Submit</button>
 					</form>
 				</div>
-
 			</div>
+			// 		<div>
+			// 			<Nav/>
+			// 			<div id="contact-form">
+			// 				<form action="./testin.php" method = "POST" onSubmit={this.handleSubmit.bind(this)}>
+			// 					<input type="text" className="form_input contact_name" name={this.state.name} onChange={this.onNameChange.bind(this)} placeholder="Name" required/>
+			// 					<input type = "text" className="form_input contact_email" name={this.state.email} onChange={this.onEmailChange.bind(this)} placeholder="Email" required/>
+			// 					<textarea type= "text" className="form-control" rows="10" name={this.state.reason} onChange={this.onReasonChange.bind(this)} placeholder="Reason for email"> </textarea>
+			// 					<button type="submit" className="btn btn-warning form_btn">Submit</button>
+			// 				</form>
+			// 			</div>
+			// 		</div>
+			// 	);
+			// }
+			//
+			// onNameChange(event) {
+			// 	this.setState({name: event.target.value})
+			// }
+			//
+			// onEmailChange(event) {
+			// 	this.setState({email: event.target.value})
+			// }
+			//
+			// onReasonChange(event) {
+			// 	this.setState({reason: event.target.value})
+			// })
 		);
 	}
 }
--- src/components/contact_form.php
+++ src/components/contact_form.php
@@ -1,37 +1,41 @@
 <?php
 //if(isset($_POST['submit'])) {
-//    $name = $_POST['name'];
-//    $email_from = $_POST['email'];
-//    $formcontent = $_POST['reason'];
+    $name = $_POST['name'];
+    $email_from = $_POST['email'];
+    $formcontent = $_POST['reason'];
+
+    $to = "brukabu@gmail.com";
+    $message = "From: $name. \n".
+        "Message: $formcontent \n";
+    $mailheader = "From: $email_from \r\n";
+    $mailheader .= "Reply-To: $name \r\n";
+//ini_set('display_errors', 'On');
+//error_reporting(E_ALL);
+//var_dump($_POST);
 //
-//    $to = "brukabu@gmail.com";
-//    $message = "From: $name. \n".
-//        "Message: $formcontent \n";
-//    $mailheader = "From: $email_from \r\n";
-//    $mailheader .= "Reply-To: $name \r\n";
-    $mail = new PHPMailer();
-
-    $mail->IsSMTP();
-    $mail->CharSet = 'UTF-8';
-
-    $mail->Host       = "https://xhomemadex.herokuapp.com"; // SMTP server example
-    $mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
-    $mail->SMTPAuth   = true;                  // enable SMTP authentication
-    $mail->Port       = 25;                    // set the SMTP port for the GMAIL server
-    $mail->Username   = "brukabu@gmail.com"; // SMTP account username example
-    $mail->Password   = "Yawn78242662130445566pop";        // SMTP account password example
-
-// Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'HomeMade - New Contact Form';
-    $mail->Body    = `From: $name \n Message: $formcontent \n`;
-    $mail->AltBody = `From: $name \n Message: $formcontent \n`;
-
+//    $mail = new PHPMailer();
+//
+//    $mail->IsSMTP();
+//    $mail->CharSet = 'UTF-8';
+//
+//    $mail->Host       = "https://xhomemadex.herokuapp.com"; // SMTP server example
+//    $mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
+//    $mail->SMTPAuth   = true;                  // enable SMTP authentication
+//    $mail->Port       = 25;                    // set the SMTP port for the GMAIL server
+//    $mail->Username   = "brukabu@gmail.com"; // SMTP account username example
+//    $mail->Password   = "Yawn78242662130445566pop";        // SMTP account password example
+//
+//// Content
+//    $mail->isHTML(true);                                  // Set email format to HTML
+//    $mail->Subject = 'HomeMade - New Contact Form';
+//    $mail->Body    = `From: $name \n Message: $formcontent \n`;
+//    $mail->AltBody = `From: $name \n Message: $formcontent \n`;
+//
+//
+//
+//
 
 
-
-
-
-//    mail($to, $subject, $message, $mailheader) or die("Error!");
+    mail($to, $subject, $message, $mailheader) or die("Error!");
 //}
 ?>

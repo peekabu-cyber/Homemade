1ab1cdc7f7a2a0f5727b2ce42184e2ec7860ba91
peekabu-cyber <61954741+peekabu-cyber@users.noreply.github.com>
2020-03-13 01:00:21 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8000
+
+app.use(express.static(__dirname + ""))
+
+app.get("/",function(req, res){
+    res.render("index");
+})
+
+app.listen(port, function(){
+
+})

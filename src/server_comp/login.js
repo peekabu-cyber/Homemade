55684d16f69b1046eba1b6b74b073917c7876426
peekabu-cyber <61954741+peekabu-cyber@users.noreply.github.com>
2020-03-13 00:26:12 -0400

    Add files via upload

---
+++ src/server_comp/login.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import Nav from './Nav';
+import {Link} from "react-router-dom";
+
+class Login extends Component{
+    render(){
+
+        return(
+            <div>
+                <Nav/>
+                <div id = "login-form" >
+                    <form method="post" action="../../server_comp/savemeals.php">
+                        <input className="form_input" type="text" name="client_username" placeholder="Enter a username" required/>
+                        <input className="form_input" type="password" name="client_password"required/>
+                        <button id="btn-savemeal" type= "submit" className="form_btn rounded mx-auto d-block btn btn-danger">Login</button>
+                    </form>
+                    <h2>Dont have an account<Link to="/signup">sign up now</Link></h2>
+                </div>
+            </div>
+        )
+    };
+}
+
+export default Login;
